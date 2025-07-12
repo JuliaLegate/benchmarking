5b9243dc8939550d8f7721c4d71804156cea991c
krasow <krasow@u.northwestern.edu>
2025-07-12 13:16:43 -0500

    update to dagger matmul for NxM

--- Dagger/matmul.jl
+++ Dagger/matmul.jl
@@ -1,21 +1,24 @@
 using CUDA
 using Dagger
+using BenchmarkTools
 
-function init_dagger(N, bs)
-  DA = rand(Blocks(bs, bs), Float32, N, N)
-  DB = rand(Blocks(bs, bs), Float32, N, N)
+function init_dagger(N, M, bs)
+  DA = rand(Blocks(bs, bs), Float32, N, M)
+  DB = rand(Blocks(bs, bs), Float32, N, M)
   DC = zeros(Blocks(bs,bs), Float32, N, N)
   return DA, DB, DC
 end
 
 N = ARGS[1]
-bs = ARGS[2]
+M = ARGS[2]
+bs = ARGS[3]
 times = []
 for i in eachindex(devices())
-  DA, DB, DC = init_dagger(N, bs)
+  DA, DB, DC = init_dagger(N, M, bs)
   t = @belapsed seconds=1000 samples=N begin
     Dagger.with_options(;scope=Dagger.scope(worker=1, cuda_gpus=$i)) do
-      mul!($DC, $DA, $DB)
+      # mul!($DC, $DA, $DB)
+      mul!(DC, DA, DB)
     end
   end
   push!(times, t)
