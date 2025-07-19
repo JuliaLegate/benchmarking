e902fc2b6c2712c5d2472e4c756ac8d2bd2914e5
Ethan Meitz <54505069+ejmeitz@users.noreply.github.com>
2025-07-18 22:11:37 -0400

    more kernel fusion

--- models/cunumeric/fused.jl
+++ models/cunumeric/fused.jl
@@ -9,7 +9,7 @@ get_time_us() = Legate.value(Legate.time_microseconds())
 
 #* THIS WILL ALLOCATE FOR F_u and F_v
 #* THE FUSED KERNEL WILL NOT HAVE THIS OVERHEAD
-#* I THINK THATS STILL FAIR? YOU KINDA OF HAVE TO DO IT THIS WAY
+#* Also time creation of F_u and F_v??
 function cuNumeric_unfused(u, v, f, k)
 
     F_u = (
@@ -33,36 +33,107 @@ end
 @inbounds function fused_kernel(u, v, F_u, F_v, N, f::Float32, k::Float32)
     
     i = (blockIdx().x - 1i32) * blockDim().x + threadIdx().x
+    j = (blockIdx().y - 1i32) * blockDim().y + threadIdx().y
 
-    if i <= N - 1 # index from 2 --> end - 1
-        u_i = u[i + 1, i + 1] 
-        v_i = v[i + 1, i + 1]
-        v_i_sq = v_i * v_i
-        F_u[i] = -u_i + v_i_sq + f*(1.0f0 - u_i)
-        F_v[i] = u_i + v_i_sq + f*(1.0f0 - u_i) - (f + k)*v_i
+    if i <= N - 1 && j <= N - 1 # index from 2 --> end - 1
+        u_ij = u[i + 1, j + 1] 
+        v_ij = v[i + 1, j + 1]
+        v_sq = v_ij * v_ij
+        F_u[i,j] = -u_ij + v_sq + f*(1.0f0 - u_ij)
+        F_v[i,j] = u_ij + v_sq + f*(1.0f0 - u_ij) - (f + k)*v_ij
     end
 
     return nothing
 end
 
-N = 1024 #* probably make this bigger
-threads = 256
-blocks = cld(N, threads)
+function run_fused(N, threads, n_steps, n_warmup)
+    
+    blocks2d = (cld(N, threads), cld(N, threads))
+    threads2d = (threads, threads)
 
-u = cuNumeric.random(Float32, (N, N))
-v = cuNumeric.random(Float32, (N, N))
-F_u = cuNumeric.zeros(Float32, (N-2, N-2))
-F_v = cuNumeric.zeros(Float32, (N-2, N-2))
+    u = cuNumeric.random(Float32, (N, N))
+    v = cuNumeric.random(Float32, (N, N))
 
-f = 0.03f0
-k = 0.06f0
+    f = 0.03f0
+    k = 0.06f0
 
-task = cuNumeric.@cuda_task kernel_add(u, v, F_u, F_v, UInt32(N), f, k)
+    task = cuNumeric.@cuda_task fused_kernel(u, v, F_u, F_v, UInt32(N), f, k)
 
-#* not sure what to do with scalars argument
-cuNumeric.@launch task=task threads=threads blocks=blocks inputs=(u, v) outputs=(F_u, F_v) scalars=UInt32(N)
+    for i in range(n_warmup)
+        #* include allocs in both
+        F_u = cuNumeric.zeros(Float32, (N-2, N-2))
+        F_v = cuNumeric.zeros(Float32, (N-2, N-2))
+        cuNumeric.@launch task=task threads=threads2d blocks=blocks2d inputs=(u, v) outputs=(F_u, F_v) scalars=(UInt32(N), f, k)
+    end
 
-Fu_cpu = F_u[:]
-Fv_cpu = F_v[:]
-println("Result of F_u after kenel launch: ", Fu_cpu[1:100])
-println("Result of F_v after kenel launch: ", Fv_cpu[1:100])
+    #* not sure what to do with scalars argument
+    start_time = get_time_us()
+    for i in range(n_steps)
+        #* include allocs in both
+        F_u = cuNumeric.zeros(Float32, (N-2, N-2))
+        F_v = cuNumeric.zeros(Float32, (N-2, N-2))
+        cuNumeric.@launch task=task threads=threads blocks=blocks inputs=(u, v) outputs=(F_u, F_v) scalars=(UInt32(N), f, k)
+    end
+    end_time = get_time_us()
+
+    total_time_μs = end_time - start_time
+    mean_time_ms = total_time_μs / (n_samples * 1e3)
+    gflops = total_flops(N, M) / (mean_time_ms * 1e6)
+
+    return mean_time_ms, gflops
+end
+
+function run_unfused(N, n_steps, n_warmup)
+    u = cuNumeric.random(Float32, (N, N))
+    v = cuNumeric.random(Float32, (N, N))
+
+    f = 0.03f0
+    k = 0.06f0
+
+    for i in range(n_warmup)
+        F_u, F_v = cuNumeric_unfused(u, v, f, k)
+    end
+
+    #* not sure what to do with scalars argument
+    start_time = get_time_us()
+    for i in range(n_steps)
+        F_u, F_v = cuNumeric_unfused(u, v, f, k)
+    end
+    end_time = get_time_us()
+
+    total_time_μs = end_time - start_time
+    mean_time_ms = total_time_μs / (n_samples * 1e3)
+    gflops = total_flops(N, M) / (mean_time_ms * 1e6)
+
+    return mean_time_ms, gflops
+end
+
+gpus = parse(Int, ARGS[1])
+N = parse(Int, ARGS[2])
+n_samples = parse(Int, ARGS[3])
+use_fused = parse(Bool, ARGS[4])
+warmup=1
+
+
+threads = 512
+
+if use_fused
+    println("[cuNumeric] Fused kernel benchmark on $(N)x$(N) matricies for $(n_samples) iterations")
+    mean_time_ms, gflops = run_fused(N, threads, n_samples, warmup)
+    println("[cuNumeric] Mean Run Time: $(mean_time_ms) ms")
+    println("[cuNumeric] FLOPS: $(gflops) GFLOPS")
+
+    open("./cudalegate/fused.csv", "a") do io
+        @printf(io, "%s,%d,%d,%d,%.6f,%.6f\n", "Legate.jl + CUDA.jl (fused)", gpus, N, M, mean_time_ms, gflops)
+    end
+else
+    println("[cuNumeric] Unfused kernel benchmark on $(N)x$(N) matricies for $(n_samples) iterations")
+    mean_time_ms, gflops = run_unfused(N, n_samples, warmup)
+
+    println("[cuNumeric] Mean Run Time: $(mean_time_ms) ms")
+    println("[cuNumeric] FLOPS: $(gflops) GFLOPS")
+
+    open("./cudalegate/unfused.csv", "a") do io
+        @printf(io, "%s,%d,%d,%d,%.6f,%.6f\n", "cuNumeric.jl (unfused)", gpus, N, M, mean_time_ms, gflops)
+    end
+end
