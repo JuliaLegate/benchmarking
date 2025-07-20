01e3fdc1f1bbad28eed953c2dcd3ba9680d9f5ac
krasow <krasow@u.northwestern.edu>
2025-07-20 17:01:31 -0500

    gc interval

--- models/cunumeric/fusion.jl
+++ models/cunumeric/fusion.jl
@@ -83,7 +83,7 @@ function run_fused(N, threads, n_samples, n_warmup)
 end
 
 function run_unfused(N, n_samples, n_warmup)
-    gc_interval = 6
+    gc_interval = 4
     u = cuNumeric.random(Float32, (N, N))
     v = cuNumeric.random(Float32, (N, N))
 
